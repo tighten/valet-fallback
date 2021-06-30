14426bb69043da5c9a20530c48f9fb962b139e40
Rissa Jackson <57572484+rissajackson@users.noreply.github.com>
2021-06-30 15:53:20 -0500

    Update styling with Tailwind css and some html

--- resources/views/components/site-in-list.blade.php
+++ resources/views/components/site-in-list.blade.php
@@ -1,15 +1,18 @@
-<li class="ml-6">
-    <strong>{{ $site->name() }}</strong>
-    @if ($site->minimumPhpVersion())
-    :: PHP <code class="text-sm">{{ $site->minimumPhpVersion() }}</code>
-    @endif
-    @if ($site::driver() === 'LaravelValetDriver')
-    :: Laravel <code class="text-sm">{{ $site->laravelVersionConstraint() }}</code>
-    @endif
+<h3 class="text-lg font-semibold">{{ $site->name() }}
+    <span class="text-base font-normal">
+        @if ($site->minimumPhpVersion())
+            :: PHP <code class="text-sm">{{ $site->minimumPhpVersion() }}</code>
+        @endif
+        @if ($site::driver() === 'LaravelValetDriver')
+            :: Laravel <code class="text-sm">{{ $site->laravelVersionConstraint() }}</code>
+        @endif
+    </span>
+</h3>
+<div class="ml-6">
     <ul class="list-disc">
     @foreach ($site->links() as $link)
         <li class="ml-6">
-        <a href="{{ $link->url() }}" class="underline text-blue-500">{{ $link->name() }}</a>
+        <a href="{{ $link->url() }}" class="underline text-purple-800">{{ $link->name() }}</a>
         {{--
     @if (! $link->secured())
         <a href="{{ route('sites.secure', $link->name()) }}" class="text-blue-500 underline">Secure</a>
@@ -20,7 +23,7 @@
     </ul>
     @switch ($site::driver())
         @case('LaravelValetDriver')
-            Laravel:<br>
+            <span class="font-semibold text-red-500">Laravel:</span><br>
             <ul class="list-disc ml-6">
                 <li>Laravel Version: <code class="text-sm">{{ $site->laravelVersionConstraint() }}</code></li>
                 <li>DB creds or link or something</li>
@@ -28,4 +31,4 @@
             </ul>
         @break
     @endswitch
-</li>
+</div>
