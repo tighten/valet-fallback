6248d7e4a743195ffc69159763e2b07371176f2e
Rissa Jackson <57572484+rissajackson@users.noreply.github.com>
2021-06-30 15:58:42 -0500

    Update styling with Tailwind css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,22 +12,38 @@
 </head>
 
 <body>
-    <div class="container mx-auto my-2 pt-6 px-4">
-        <h1 class="text-3xl mb-4 border-b-2">My sites</h1>
-
-        <h2 class="text-2xl mt-6 uppercase">Linked</h2>
-        <ul class="list-disc">
+    <nav class="bg-gradient-to-b from-purple-500 to-purple-700 p-6">
+        <div class="max-w-7xl mx-auto">
+            <div class="flex items-center flex-shrink-0 text-white mr-6">
+                <p class="font-semibold text-xl tracking-tight">Laravel
+                    <span class="font-extralight">Valet</span>
+                </p>
+            </div>
+        </div>
+    </nav>
+    <nav class="bg-white p-6 border-b border-gray-200 shadow">
+        <div class="max-w-7xl mx-auto">
+            <p class="text-3xl">Did you mean
+                <span class="font-semibold text-purple-800 underline">symposium</span>
+            </p>
+        </div>
+    </nav>
+    <div class="p-4 bg-gray-100">
+        <div class="max-w-7xl mx-auto">
+            <h2 class="text-2xl mt-6 font-semibold font-sans">Linked</h2>
             @foreach ($linked as $site)
-                <x-site-in-list :site="$site"></x-site-in-list>
+                <div class="bg-white mt-4 p-6 rounded-md border border-gray-200 shadow">
+                    <x-site-in-list :site="$site"></x-site-in-list>
+                </div>
             @endforeach
-        </ul>
 
-        <h2 class="text-xl mt-6 uppercase">Parked</h2>
-        <ul class="list-disc">
+            <h2 class="text-2xl mt-10 font-semibold font-sans">Parked</h2>
             @foreach ($parked as $site)
-                <x-site-in-list :site="$site"></x-site-in-list>
+                <div class="bg-white mt-4 p-6 rounded-md border border-gray-200 shadow">
+                    <x-site-in-list :site="$site"></x-site-in-list>
+                </div>
             @endforeach
-        </ul>
+        </div>
     </div>
 </body>
 </html>
